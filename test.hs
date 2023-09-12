8333eefdb45374179b3d01e326799e60db7b8d73
NadiaBarco <48421521+NadiaBarco@users.noreply.github.com>
2023-09-11 21:40:38 -0300

    Update test.hs
    Solo ordenado por ejercicio 
    Falta ejercicio guia 4 y guía 5 recién empezada

--- test.hs
+++ test.hs
@@ -1,3 +1,8 @@
+--Ejercicio 1.a
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+--import qualified Main as 4
+{-# HLINT ignore "Use even" #-}
+{-# HLINT ignore "Use uncurry" #-}
 f:: Integer -> Integer
 f 1 =8
 f 4 = 131
@@ -9,80 +14,95 @@ f11 x | x== 1 =8
      | x==16 = 16
      | otherwise=0
 
+--Ejercicio 1.b
 g:: Integer -> Integer
 g 8 = 16
 g 16 = 4
 g 131 = 1
 
+--Ejercicio 1.c
 h :: Integer -> Integer
 h x = f11 (g x)
 
 h2:: Integer -> Integer
 h2 x = g (f11 x)
 
-
+--Ejercicio 2.a
 absoluto:: Integer -> Integer
 absoluto n | n<0 = -n
            |otherwise = n
 
+--Ejercicio 2.b
 maximoAbsoluto:: Integer -> Integer -> Integer
 maximoAbsoluto n m | absoluto n >= absoluto m =absoluto n
                    | otherwise = absoluto m
 
+--Ejercicio 2.c
 maximo3 :: Integer -> Integer -> Integer -> Integer
 maximo3 n m l | n >= m && n>= l = n
               | m > n && m>= l = m
               |otherwise= l
 
-
+--Ejercicio 2.d
 algunoEs0 :: Integer -> Integer -> Bool
 algunoEs0 _ 0 =True
 algunoEs0 0 _ =False
 
+--Ejercicio 2.e
 ambosSon0 :: Integer -> Integer -> Bool
 ambosSon0 0 0 = True
 
+--Ejercicio 2.f
 mismoInterval0 :: Float -> Float -> Bool
 mismoInterval0 n m = n <= 3 && m<=3 || n > 3 && m >3 || n >7 && m > 7
 
+--Ejercicio 2.g
+
 sonIguales :: Integer -> Integer ->Bool
 sonIguales n m = n == m
+
 sunaDistintos :: Integer -> Integer -> Integer -> Integer
 sunaDistintos n m l | sonIguales n m && sonIguales n l  = 0
                     | sonIguales n m  || sonIguales m l= n + l
                     | otherwise = n +m +l
 
+--Ejercicio 2.h
 esMultiploDe :: Integer-> Integer ->Bool
 esMultiploDe n m = n `mod` m == 0
 
+--Ejercicio 2.i
 digitoUnidades:: Integer -> Integer
 digitoUnidades n = if n < 10 then n else n `mod` 10
 
+--Ejercicio 2.j
 digitoDecenas :: Integer ->Integer
 digitoDecenas n = n `div` 10 `mod` 10
 
 
---
+--Ejercicio 3
 estanRelacionados::Integer -> Integer -> Bool
 estanRelacionados n m |n * n + n * m * k == 0 && k /= 0 =True
                       | otherwise = False
                       where k = (-n) `div` m
 
 
---
+----Ejercicio 4.a
 prodInt:: (Float,Float) -> (Float,Float) -> Float
 prodInt n m = fst n * fst m + snd n* snd m
 
+--Ejercicio 4.b
 todoMenor:: (Float,Float) ->(Float,Float) -> Bool
 todoMenor n m = fst n < fst m && snd n < snd m
 
+--Ejercicio 4.c
 distanciaPuntos :: (Float,Float) -> (Float,Float) -> Float
 distanciaPuntos n m = sqrt ((fst m- fst n)**2+ (snd m - snd n)**2)
 
+--Ejercicio 4.d
 sumaTerna:: (Float,Float,Float) -> Float
 sumaTerna (x,y,z) = x+y+z
 
-{-
+----Ejercicio 4.e
 sumarSoloMultiplos :: (Integer,Integer,Integer)-> Integer -> Integer
 sumarSoloMultiplos t n | not (esMultiploDe (fst t) n) && not (esMultiploDe (snd t) n) && not (esMultiploDe (thr t) n)=0
                              | esMultiploDe (fst t)  n && esMultiploDe (snd t) n && not (esMultiploDe (thr t) n)= fst t + snd t
@@ -92,26 +112,29 @@ sumarSoloMultiplos t n | not (esMultiploDe (fst t) n) && not (esMultiploDe (snd 
                                    fst (x,_,_) = x
                                    snd (_,y,_) = y
                                    thr (_,_,z) = z
-                                   
+--Ejercicio 4.f                                   
 posPrimerPar :: (Integer,Integer,Integer) -> Integer
 posPrimerPar (x,y,z) | x `mod` 2 == 0 = 0
                      | y `mod` 2 == 0 = 1
                      | z `mod` 2 == 0 = 2
                      |otherwise =4
-
+{-
+--Usando una funcion predefinida, innecesario (?, no se pueden usar funciones pre definidas que no esten en la teorica
 posPrimerPar1 :: (Integer,Integer,Integer) -> Integer
 posPrimerPar1 (x,y,z) | even x  = 1
                       | even y = 2
                       | even z = 3
                       | otherwise = 4
 -}
+--Ejercicio 4.g
 crearPar:: a-> b -> (a,b)
 crearPar x y = (x,y)
 
+--Ejercicio 4.h
 invertir :: (a,b) -> (b,a)
 invertir (x,y)=(y,x)
 
---
+--Ejercicio 5
 fMenores :: Integer -> Integer
 fMenores n = if n <= 7 then n*n else 2*n -1
 
@@ -121,39 +144,41 @@ gMenores n = if even n then n `div`2 else 3*n+1
 todosMenores :: (Integer, Integer, Integer) ->Bool
 todosMenores (x,y,z) = fMenores x > gMenores x && fMenores y > gMenores y && fMenores z > gMenores z
 
---
+--Ejercicio 6
 bisiesto :: Integer -> Bool
 bisiesto año = not (año `mod` 4 /=0 || año `mod` 100 ==0 && año `mod` 400 /=0)
---
 
 
+--Ejercicio 7
 distanciaManhattan :: (Float, Float, Float) ->(Float, Float, Float) ->Float
 distanciaManhattan (x,y,z) (x2,y2,z2) = abs (x-x2) + abs (y-y2) + abs (z-z2)
 
----
-
+--Ejercicio 8
 comparar:: Integer -> Integer -> Integer
 comparar n m |  mod n 10 + mod (div n  10) 10 > mod m 10 + mod (div m  10) 10 = - 1
              | mod n 10 + mod (div n  10) 10 < mod m 10 + mod (div m  10) 10 = 1
              | otherwise = 0
 
 
-------------GUIA 4-----------
+------------GUIA 4 RECURSION SOBRE NUMEROS ENTEROS-----------
 
+--Ejercicio 1
 fibonacci:: Integer -> Integer
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci n = fibonacci (n-2) + fibonacci (n-1)
 
+--Ejercicio 2
+-- Se supone que se utilice recursion, REHACER
 parteEnrtera :: Float -> Integer
 parteEnrtera = round
 
-
+--Ejercicio 3
 esDivisible:: Integer -> Integer -> Bool
 esDivisible n m | m == n = False
                 | otherwise = esDivisible n (m-n)
 
---
+--Ejercicio 4
 sumaImpares:: Integer -> Integer
 sumaImpares n = sumaImparesAux n 1 0
 
@@ -162,6 +187,7 @@ sumaImparesAux n m c|  c == n = 0
                     |odd m = m + sumaImparesAux n (m+1) (c+1)
                     |otherwise = sumaImparesAux n (m+1) c
 ------------REHACER----------LISTO!!
+--Ejercicio 5
 medioFact:: Integer -> Integer
 medioFact 0 = 1
 medioFact 1 = 1
@@ -177,10 +203,12 @@ factorial n = n*factorial (n-1)
 --              |otherwise= mod n 10 + sumaDigitos (mod (div n 10) 10) 
 --funciona, pero en dos pasos ya llego a mi caso base, se deberia sacar el mod 
 
+--Ejercicio 6
 sumaDigitos :: Integer -> Integer
 sumaDigitos n | n< 10 = n
               |otherwise= mod n 10 + sumaDigitos (div n 10)
 
+--Ejercicio 7
 todosDigitosIguales:: Integer -> Bool
 todosDigitosIguales n = todosDigitosIgualesRec n == n
 
@@ -191,17 +219,17 @@ todosDigitosIgualesRec n | mod n  10 == n = n
                          | mod n 10 == todosDigitosIgualesRec (div n 10) = n
                          | otherwise = 0
 
---
+--Ejercicio 8
 iesimoDigito :: Integer -> Integer -> Integer
 iesimoDigito n i| n < 10 = n
                 | i< n && i == cantidadDeDigitos n = mod n 10
                 | otherwise = iesimoDigito (div n 10) i
 
-
 cantidadDeDigitos :: Integer -> Integer
 cantidadDeDigitos n | n < 10 = 1
                     | otherwise = 1 + cantidadDeDigitos (div n 10)
 
+--Ejercicio 9
 esCapicua:: Integer -> Bool
 esCapicua n = n == invertirNumero n (cantidadDeDigitos n)
 
@@ -209,17 +237,17 @@ invertirNumero :: Integer->Integer -> Integer
 invertirNumero n d | n < 10 = n
                    | otherwise = mod n 10*(10^(d-1 )) + invertirNumero ( div n 10) (d -1)
 
---
-
+--Ejercicio 10.a
+--Auxiliar para el indice
 f1Rec :: Integer -> Integer ->Integer
 f1Rec 0 _ = 1
 f1Rec n i | i == n = 2^i
           |otherwise= 2^i + f1Rec n (i+1)
---Auxiliar--
+
 fUno :: Integer -> Integer
 fUno n = f1Rec n 0
 --
-
+--Ejercicio 10.b
 f2 :: Integer -> Integer -> Integer
 f2 n q = f2Aux n q 1
 
@@ -228,13 +256,15 @@ f2Aux _ 0 _ = 0
 f2Aux n q i | i == n = q^i
             | otherwise = q^i + f2Aux n q (i+1)
 
+--Ejercicio 10.c
 f3:: Integer -> Integer -> Integer
 f3 n q = f2Aux (2*n) q 1
 
+--Ejercicio 10.d
 f4 :: Integer -> Integer -> Integer
 f4 n q = f2Aux (2*n) q n
 
---
+--Ejercicio 11.a
 eAprox :: Integer -> Float
 eAprox 0 = 1
 eAprox n =  eAproxIndice n 0
@@ -243,10 +273,11 @@ eAproxIndice::Integer -> Integer -> Float
 eAproxIndice 0 _ = 1.0
 eAproxIndice n i | n ==i = 1 / fromIntegral (factorial i)
                  | otherwise = 1/fromIntegral (factorial i) + eAproxIndice n (i+1)
---
+
+----Ejercicio 11.b
 fe = eAprox 10
 
---
+--Ejercicio 12
 raizDe2Aprox:: Integer -> Float
 raizDe2Aprox 1 = 1
 raizDe2Aprox n = sucecionAproxRaizDe2 n -1
@@ -331,7 +362,7 @@ enesimoPrimo n = enesimoPrimoCont n 2 0
 
 enesimoPrimoCont:: Integer -> Integer -> Integer -> Integer
 enesimoPrimoCont 1 p c = 2
-enesimoPrimoCont n p c | n == c && esPrimo(p) = p
+enesimoPrimoCont n p c | n == c && esPrimo p = p
                        | otherwise = enesimoPrimoCont n (p+1) (c+1)
 ------
 
@@ -340,8 +371,8 @@ esFibonacci 1 = True
 esFibonacci n = perteneceAFibo n 1
 
 perteneceAFibo :: Integer -> Integer -> Bool
-perteneceAFibo n m | n == fibonacci(m) = True
-                    | n < fibonacci (m) = False
+perteneceAFibo n m | n == fibonacci m = True
+                    | n < fibonacci m = False
                    | otherwise= perteneceAFibo n (m+1)
 
 
@@ -353,16 +384,18 @@ mayorDivisor::Integer -> Integer -> Integer -> Integer
 mayorDivisor 1 _ _= 1
 mayorDivisor n d c | mod n d == 0 && n == c = d
                    | mod n d /=0 = mayorDivisor n (d+1) (c+1)
-                   | otherwise = mayorDivisor n (d+1) (c)
+                   | otherwise = mayorDivisor n (d+1) c
 
 
 
 ----------------------GUIA 5 RECURSION SOBRE LISTAS------------------
 
+--Ejercicio 1.1
 longitud :: [t] -> Integer
 longitud [] = 0
 longitud l = 1 + longitud (tail l)
 
+--Ejercicio 1.2
 ultimo :: [t] -> t
 ultimo [x] = x
 ultimo x = ultimo (tail x)
@@ -378,24 +411,25 @@ cantidadDeApariciones _ [] = 0
 cantidadDeApariciones e (x:xs) | pertenece e (x:xs) = 1
                                | 
 --}
-
+--Ejercicio 1.4
 reverso :: [t] -> [t]
 reverso [x] = [x]
-reverso (x:xs) = reverso (xs) ++ [x]
+reverso (x:xs) = reverso xs ++ [x]
 
----Ejercicio 2 --
+---Ejercicio 2.1 --
 pertenece::(Eq t) => t -> [t] -> Bool
 pertenece _ [] = False
 pertenece e (x:xs) = e == x || pertenece e xs
-                   
+
+--Ejercicio 2.2
 todosIguales:: (Eq t) => [t] -> Bool
 todosIguales [x] = True
 todosIguales (x:xs) | x /= head xs = False
                     | otherwise= todosIguales xs 
 
+--Ejercicio 2.3
 todosDistintos:: (Eq t) => [t] -> Bool
 todosDistintos [x] = True
 todosDistintos (x:xs)| x == head xs = False
                      | otherwise = todosDistintos xs
 -------------
-
